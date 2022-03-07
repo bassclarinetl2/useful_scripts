dd200f03d9b18d85f2d2b0fc3dc72434b723c64a
Will <bassclarinetl2@gmail.com>
2022-03-07 23:34:15 +0000

    edit gitignore and add exclude to unused ips sh

--- bash/scanunusedips.sh
+++ bash/scanunusedips.sh
@@ -7,7 +7,8 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 #Scan for "down" hosts
-sudo nmap -v -sn -n 192.168.2.1/23 -oG - | awk '/Status: Down/{print $2}' > unusedhosts.txt
+sudo nmap -v -sn -n 192.168.2.1/24 --exclude 192.168.2.0/32,192.168.2.100/30,192.168.2.104/29,192.168.2.112/28,192.168.2.128/26,192.168.2.192/27,192.168.224/28,192.168.240/29,192.168.248/32,192.168.250/32 -oG - | awk '/Status: Down/{print $2}' > unusedhosts.txt
+chmod 777 unusedhosts.txt
 
 exit 0
 
