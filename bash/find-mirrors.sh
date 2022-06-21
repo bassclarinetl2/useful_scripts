6d9ba5906447ae8b0236e9256ffa019fc5dceeec
Will <bassclarinetl2@gmail.com>
2022-06-21 23:18:58 +0000

    add additional scripts and new stylesheet for nmap

---
+++ bash/find-mirrors.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+# Requires: Bash, GNU grep and parallel, htmlq, and httpie or xh.
+# Usage: 
+#        ./find-mirrors.sh ARCH DISTRO REPOSITORY PROTOCOL JOBS
+# Example: 
+#        ./find-mirrors.sh armhf focal main https 6
+#
+# Copyright 2022 Alex DeLorenzo. Licensed under the GPLv3.
+# Available: https://gist.githubusercontent.com/alexdelorenzo/8cdb21718c2d2d3f5f8beaad0bf6c843/raw/f8f9bf73bd4a55cdde25febe7a0be276011ae33c/find-mirrors.sh
+#
+export ARCH="${1:-amd64}"
+export DISTRO="${2:-focal}"
+export REPOSITORY="${3:-main}"
+export PROTOCOL="${4:-http}"
+export JOBS="${5:-4}"
+
+export URL_PATH="dists/$DISTRO/$REPOSITORY/binary-$ARCH/"
+export LIST_URL="https://launchpad.net/ubuntu/+archivemirrors/"
+export SELECTOR="#mirrors_list > tbody > tr > td:nth-child(2) > a"
+export TIMEOUT=10
+
+export RC_MISSING_DEPS=1
+export NO_PARALLEL="GNU parallel is missing, installing with apt...\n"
+export NO_HTMLQ="You need to install htmlq: https://github.com/mgdm/htmlq\n"
+
+set -eu
+shopt -s expand_aliases
+
+alias installHttp='python3 -m pip install --upgrade httpie'
+
+
+quiet() {
+  "$@" &> /dev/null
+}
+export -f quiet
+
+
+exists() {
+  quiet which $@
+}
+
+
+getDependencies() {
+  exists parallel || {
+    printf "%s" "$NO_PARALLEL"
+    sudo apt install parallel
+  }
+
+  exists xh || exists http || {
+    installHttp || return $RC_MISSING_DEPS
+  }
+
+  export http=http
+  exists xh && export http=xh
+
+  exists htmlq || {
+    printf "%s" "$NO_HTMLQ"
+    return $RC_MISSING_DEPS
+  }
+}
+
+
+getMirrors() {
+  $http --body --follow get "$LIST_URL" \
+    | htmlq "$SELECTOR" --attribute href \
+    | grep -i "$PROTOCOL:"
+}
+
+
+checkRepo() {
+  local url="$1"
+
+  $http get "$url" \
+    --quiet --quiet \
+    --timeout $TIMEOUT \
+    --verify=no \
+    --ignore-stdin \
+    --follow \
+    --headers \
+    --check-status
+}
+export -f checkRepo
+
+
+testMirror() {
+  local url="$1"
+  local repoUrl="$url/$URL_PATH"
+
+  checkRepo "$repoUrl" &&
+    printf "Valid: %s\n" "$url"
+}
+export -f testMirror
+
+
+testMirrors() {
+  parallel --will-cite --jobs "$JOBS" testMirror
+}
+
+
+main() {
+  getDependencies || return $RC_MISSING_DEPS
+  getMirrors | testMirrors
+}
+
+
+main
--- bash/nmap_scan.sh
+++ bash/nmap_scan.sh
@@ -8,10 +8,13 @@ fi
 
 
 ##Scan hosts
-nmap -O -oX nmapscan.txt 192.168.2.0/23 --webxml --dns-servers 192.168.2.2
+nmap -O -oX nmapscan.txt 192.168.2.0/23 --stylesheet  https://githubusercontent.com/honze-net/nmap-bootstrap-xsl/master/nmap-bootstrap.xsl --dns-servers 192.168.2.2
 
 #convert hosts to html
 xsltproc nmapscan.txt -o hosts.html
+
+#copy hosts.html to /var/www/html
+cp hosts.html /var/www/html
 
 ##Look for linux hosts output to linuxhosts.txt
 echo "xpath //host[status[@state='up'] and os[osmatch/osclass[@osfamily='Linux']]]/address[@addrtype='ipv4']/@addr" | xmllint --shell nmapscan.txt | grep "content=" | awk -F= '{ print $2 }' > linuxhosts.txt
@@ -25,5 +28,5 @@ echo "xpath //host[status[@state='up'] and os[osmatch/osclass[@osfamily='Linux']
 
 ##Look for raspis
 
-piscan.sh
+./piscan.sh
 exit 0
