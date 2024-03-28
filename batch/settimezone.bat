af02450b14c3c3d7c7895ea7541f9d1a98324463
Will Heid <bassclarinetl2@users.noreply.github.com>
2024-03-28 11:34:39 -0700

    Update settimezone.bat

--- batch/settimezone.bat
+++ batch/settimezone.bat
@@ -1,1 +1,22 @@
+@echo off
+SETLOCAL
+SET /P TZ="Press 1 for Pacific Standard Time or 2 for Pacific Daylight Time"
 
+2>NUL CALL :CASE_%TZ% # jump to :case_1, case_2 etc
+IF ERRORLEVEL 1 CALL :DEFAULT_CASE # if no label
+
+ECHO Done
+EXIT /B
+
+:CASE_1
+  tztuil /S "Pacific Standard Time"
+  GOTO END_CASE
+:CASE_2
+  tzutil /S "Pacific Daylight Time"
+  GOTO END_CASE
+:DEFAULT_CASE
+  ECHO Unknown option %TZ%
+  GOTO END_CASE
+:END_CASE
+  VER > NUL # reset ERRORLEVEL
+  GOTO :EOF # reset from call
